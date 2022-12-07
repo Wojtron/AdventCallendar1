0f07ed858be0d250c56b70edcca2e152d129443d
Wojtron <wojciechwrobel1994@gmail.com>
2022-12-07 03:09:55 +0100

    Probably almost done, but have to check if it works properly

--- main.py
+++ main.py
@@ -3,8 +3,95 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def welcome():
+    greet = "Hello Elf! \n Here is your manager, where you should input calories of every meal you'll eat. \n"
+    choice = input("Do you want to create new list or display sample list? (type 'new' or 'sample' : ")
+    choiceFlag = False
+    while(choiceFlag == False):
+        if choice == "new" :
+            choiceFlag = True
+            userList()
+        elif choice == "sample" :
+            choiceFlag = True
+            sampleList()
+        else:
+            print("Sorry i didn't understood :( ")
+
+
+def userList():
+
+    user = []
+    userSingeElf = 0
+    userSum =[]
+    print("Ok! Remember, that every empty line means equipment of next Elf!")
+
+    def addToList():
+        newCalories = input()
+        user.append(newCalories)
+
+    def showList():
+
+        elfNumber = 0
+        if user == None:
+            print("New list hasn't beet created yet. ")
+        else:
+            for x in user:
+                print(user[x])
+                if isinstance(user[x],int):
+                    print(user[x])
+                    userSingeElf = userSingeElf+user[x]
+                else:
+                    elfNumber = elfNumber+ 1
+                    print("Elf number" + elfNumber + "equipment")
+                    userSum.append(userSingeElf)
+                    userSingeElf = 0
+        print("sum of Elfs meals calories: " + sum(userSum))
+
+
+
+
+
+
+
+ #            for x in user:
+ #               print(user[x])
+  #              if isinstance(user[x], int):
+   #                 print("Elf Number" + elfCounter + ":   \n" + singleElf + "\n\n" )
+    #                elfCounter = elfCounter + 1
+#
+ #               else
+
+
+  #              elif x == len(user):
+   #                 print(allElfs)
+
+
+
+
+
+
+    def editList():
+        showList()
+        editedPosition = input("Which position should be edited? ")
+        editedAmount = input ("What amount should be instead?")
+
+        user[int(editedPosition)] = str(editedAmount)
+
+
+    def getBack():
+        return
+
+def sampleList():
+
+    sample = ["1000","2000","3000","","4000","","5000","6000","","7000","8000","9000","1000"]
+    print("Here is your list")
+
+
 
 def print_hi(name):
+    welcome()
+
+
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
